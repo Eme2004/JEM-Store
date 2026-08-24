1f6f565ddb2592bcf0953267a840a6e6769570f3
Eme2004 <emmairenase@est.utn.ac.cr>
2026-08-23 21:33:44 -0600

    Actualizacion de routes

--- routes/web.php
+++ routes/web.php
@@ -112,10 +112,12 @@ Route::middleware('auth')->group(function () {
     // Reportes de ventas
     // --------------------------------------------------
 
-    Route::get('/reportes', [ReportController::class, 'index'])
+    Route::middleware('admin')->group(function () {
+
+        Route::get('/reportes', [ReportController::class, 'index'])
         ->name('reports.index');
 
-    Route::get('/reportes/pdf', [ReportController::class, 'pdf'])
+        Route::get('/reportes/pdf', [ReportController::class, 'pdf'])
         ->name('reports.pdf');
 
 
@@ -145,4 +147,6 @@ Route::middleware('auth')->group(function () {
 
     });
 
+
+    });
 });
